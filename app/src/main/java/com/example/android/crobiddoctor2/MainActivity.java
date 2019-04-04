419357814fae9bbb13c3dd36f6b92791f1c92b4d
Rohan Arora <rohanjai777@gmail.com>
2019-04-05 00:41:28 +0530

    App

---
+++ app/src/main/java/com/example/android/crobiddoctor2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.crobiddoctor2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void next(View view) {
+        Intent intent = new Intent(this, test.class);
+        startActivity(intent);
+    }
+}
